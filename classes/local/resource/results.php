8c1a58ce8fa74e921a714522d91e8c82d3b14dbe
Vervoort, Claude <claude.vervoort@cengage.com>
2017-11-01 16:25:18 -0400

    blank URL when no linked gb column

--- classes/local/resource/results.php
+++ classes/local/resource/results.php
@@ -204,21 +204,19 @@ EOD;
         if (strpos($value, '$Results.url') !== false) {
             require_once($CFG->libdir . '/gradelib.php');
 
+            $resolved = '';
             $this->params['context_id'] = $COURSE->id;
             $id = optional_param('id', 0, PARAM_INT); // Course Module ID.
-            try {
                 if (!empty($id)) {
                     $cm = get_coursemodule_from_id('lti', $id, 0, false, MUST_EXIST);
                     $id = $cm->instance;
+                    $item = grade_get_grades($COURSE->id, 'mod', 'lti', $id);
+                    if ($item && $item->items) {
+                        $this->params['item_id'] = $item->items[0]->id;
+                        $resolved = parent::get_endpoint();
+                    }
                 }
-                $item = grade_get_grades($COURSE->id, 'mod', 'lti', $id);
-                if ($item && $item->items) {
-                    $this->params['item_id'] = $item->items[0]->id;
-                }
-            } catch (\Exception $e) {
-                $this->params['item_id'] = 0;
-            }
-            $value = str_replace('$Results.url', parent::get_endpoint(), $value);
+            $value = str_replace('$Results.url', $resolved, $value);
         }
 
         return $value;
